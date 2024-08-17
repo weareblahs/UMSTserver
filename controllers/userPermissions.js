7c550c6dee055d4494c4358d4c6ce69787a491e2
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2024-08-17 12:05:13 +0800

    add user authentication information GET endpoint

--- controllers/userPermissions.js
+++ controllers/userPermissions.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const UAI = require("../mongodb_models/UserAuthenticationInformation");
 const auth = require("../middleware/auth");
 const User = require("../mongodb_models/User");
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const data = await UAI.find({ user: req.user._id });
     res.json(data);
@@ -18,7 +18,8 @@ router.post("/", async (req, res) => {
     const user = req.body.user;
     const userData = await User.find({ username: user });
     const existingID = await UAI.find({ userID: userData[0].id });
-    if (existingID) {
+    console.log(existingID);
+    if (existingID.length > 0) {
       res.status(400).json({
         error:
           "ID exists. Please use the appropriate update endpoints to update the status of the user.",
